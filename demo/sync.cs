df32d5041aa22e7c6f99c29484cfd661db6dd6b1
lihebin <include_lihebin@163.com>
2017-10-21 22:08:13 +0800

    c# demo

---
+++ demo/sync.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.Windows.Documents;
+using System.Security.Cryptography;
+
+namespace shouqianba_sync_csharpdemo.demo
+{
+    class sync
+    {
+        public static string api_domain = "https://api.shouqianba.com";
+        public static JObject syncdemo(string terminal_sn, string terminal_key)
+        {
+            string url = api_domain + "/api/sync/order";
+
+            Dictionary<string, object> item = new Dictionary<string, object>();
+            item.Add("item_no", "");  //商品税收编码
+            item.Add("item_name", ""); //发票项目名称（或商品名称）
+            item.Add("quantity", "");  //数量, 默认为1。
+            item.Add("row_type", "");  //发票行性质
+            item.Add("specification", "");  //规格型号,可选
+            item.Add("unit", "");  //单位
+            item.Add("amount", "");  //价税合计金额，单位为分
+            item.Add("tax_rate", "");   //税率
+            item.Add("zero_rate_flag", "");  //0税率标识
+            List<object> items = new List<object>();
+            items.Add(item);
+
+
+            JObject Jparams = new JObject();
+
+            Jparams.Add(new JProperty("terminal_sn", terminal_sn)); //终端号
+            Jparams.Add(new JProperty("client_sn", ""));   //商户系统订单号
+            Jparams.Add(new JProperty("client_task_sn", ""));  //商户系统订单流水号
+            Jparams.Add(new JProperty("client_time", 123));   //商户系统订单完成时间
+            Jparams.Add(new JProperty("order_type", ""));  //订单类型
+            Jparams.Add(new JProperty("client_original_sn", ""));  //原始订单号
+            Jparams.Add(new JProperty("amount", ""));   //订单金额
+            Jparams.Add(new JProperty("invoice_items", items));  //开票商品明细信息
+            string sign = getSign(Jparams.ToString() + terminal_key);
+            string result = httpUtil.httpPost(url, Jparams.ToString(), sign, terminal_sn);
+            JObject retObj = JObject.Parse(result);
+
+            string resCode = retObj["result_code"].ToString();
+            Console.WriteLine("返回码：" + resCode);
+            if (resCode.Equals("200"))
+            {
+
+                string responseStr = retObj["biz_response"].ToString();
+                JObject terminal = JObject.Parse(responseStr);
+                Console.WriteLine("返回信息:" + terminal);
+                return terminal;
+            }
+            else
+            {
+                string errorMsg = retObj["error_message"].ToString();
+                Console.WriteLine("返回信息：" + errorMsg);
+            }
+            return null;
+        }
+
+
+        private static string getSign(string signStr)
+        {
+            string md5 = MD5(signStr);
+            return md5;
+        }
+        public static string MD5(string value)
+        {
+            StringBuilder result = new StringBuilder("");
+            string cl1 = value;
+            System.Security.Cryptography.MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] s = md5.ComputeHash(Encoding.GetEncoding("UTF-8").GetBytes(cl1));
+            for (int i = 0; i < s.Length; i++)
+            {
+                result.AppendFormat("{0:x2}", s[i]);
+            }
+            return result.ToString();
+        }
+    }
+}
